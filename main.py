9cec52dde12a091626791b9567a71948be80bfbd
August Almgren <august.almgren@gmail.com>
2017-10-09 15:59:12 +0200

    Bundled pygame to fix dependency issues
    Added in pygame so game so users don't need to setup include paths

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import sys
+sys.path.append('src')
+sys.path.append('src/enemy')
+sys.path.append('src/level')
+sys.path.append('src/player')
+sys.path.append('pygame')
+import game 
+game.run()
